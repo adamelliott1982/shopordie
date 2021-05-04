0e644bffdce028660ec1b19f0c3895d52055fdf8
Adam Elliott <adamelliott1982@gmail.com>
2021-05-04 14:23:38 -0500

    shop or die version 1 complete

--- script.js
+++ script.js
@@ -5,6 +5,25 @@ character = {
     zombieAware: false,
     haveWeapon: false,
 }
+
+//AUDIO
+let static = document.getElementById('static');
+let doorOpen = document.getElementById('door-open');
+let doorClose = document.getElementById('door-close');
+let birds = document.getElementById('birds');
+let knock = document.getElementById('knock');
+let zombieBreath = document.getElementById('zombie-breath');
+let zombieGroan = document.getElementById('zombie-groan');
+let fryingPan = document.getElementById('frying-pan');
+let steps1 = document.getElementById('steps-1');
+let steps2 = document.getElementById('steps-2');
+let cat = document.getElementById('cat');
+let injury = document.getElementById('injury');
+let metalDoor = document.getElementById('metal-door');
+let upgrade = document.getElementById('upgrade');
+let splat = document.getElementById('splat');
+let victory = document.getElementById('victory');
+
 //VARIABLE ASSIGNMENTS
 let hpContainer = document.getElementById('hp-container');
 let hpCount = document.getElementById('hp-count')
@@ -41,7 +60,7 @@ let headDownstairs = document.getElementById('head-downstairs');
 //STREET
 let streetLevel = document.getElementById('street-level');
 let streetText = document.getElementById('street-text');
-let headInside = document.getElementById('button-container-2');
+let headInside = document.getElementById('head-inside');
 let headGroceryStore = document.getElementById('head-grocery-store');
 //WALKING
 let walkingLevel = document.getElementById('walking-level');
@@ -89,12 +108,13 @@ let findMedicineText = document.getElementById('find-medicine-text');
 //END LEVEL
 let endLevel = document.getElementById('end-level')
 let complete = document.getElementById('complete')
+let score = document.getElementById('score');
 
 //function for title case found on stackoverflow
 function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
-//combat methods
+/////////////////////////////////////////////////////////////combat methods
 function attackGiven(){
     attackContainer.style.display = 'flex';
     attackContainer.style.animation = 'fade-out 2s ease 2s forwards';
@@ -116,9 +136,10 @@ function damageTaken(){
     damageContainer.style.animation = 'fade-out 2s ease 2s forwards'
     character.hp -= 1;
     hpCount.innerText = character.hp
+    injury.play()
 }
 
-//combat events
+////////////////////////////////////////////////////////////////combat events
 fight1.addEventListener('click', function(){
     if (character.haveWeapon == true){
         attackGiven()
@@ -127,6 +148,8 @@ fight1.addEventListener('click', function(){
         searchAlley.style.display = 'block';
         alleyH1.innerText = "Tango Down!"
         alleyText.innerText = "Thank goodness you found this frying pan..."
+        fryingPan.play()
+        zombieGroan.pause()
     } else {
         damageTaken()
         console.log(character.hp);
@@ -134,6 +157,7 @@ fight1.addEventListener('click', function(){
             alert("You are critically low on health!  You should find a weapon to take on these zombies!")
         }
         if (character.hp <= 0) {
+            splat.play()
             console.log('You are dead');
             alleyLevel.style.display = 'none'
             gameOver.style.opacity = 0
@@ -149,10 +173,16 @@ fight2.addEventListener('click', function(){
         fight2.style.display = 'none';
         pharmacyH1.innerText = "Tango Down!"
         pharmacyText.innerText = "Thank goodness you found this frying pan..."
+        fryingPan.play()
+        zombieBreath.pause()
     } else {
         damageTaken()
         console.log(character.hp);
+        if (character.hp == 1){
+            alert("You are critically low on health!  You should find a weapon to take on these zombies!")
+        }
         if (character.hp <= 0) {
+            splat.play()
             console.log('You are dead');
             pharmacy.style.display = 'none'
             gameOver.style.opacity = 0
@@ -162,13 +192,15 @@ fight2.addEventListener('click', function(){
         }
     }
 })
-//set up opening scene
+
+//////////////////////////////////////////////////////////////set up opening scene
 acceptNameButton.addEventListener('click', function(){
     charName = charNameField.value
     characterCreation.style.display = 'none';
     cityLevel.style.display = 'flex';
     cityLevel.style.animation = 'fade-in 2s ease forwards'
-    greetingText.innerText = `Good morning ${capitalizeFirstLetter(charNameField.value)}, you've awoken to the sound of the birds singing, the sun shining...and a disturbing feeling deep within the pit of your stomach...`;  
+    greetingText.innerText = `Good morning ${capitalizeFirstLetter(charNameField.value)}, you've awoken to the sound of the birds singing, the sun shining...and a disturbing feeling deep within the pit of your stomach...`;
+    birds.play()
 })
 
 //title-screen
@@ -180,28 +212,35 @@ play.addEventListener('click', function(){
     hpContainer.style.display = 'flex'
     hpContainer.style.animation = 'fade-in 2s ease forwards';
     buttonContainer1.style.animation = 'fade-in 1s ease 1s forwards'
+    zombieGroan.play()
 })
 
-//levels
+////////////////////////////////////////////////////////////////////////levels
 //city-level
 checkNews.addEventListener('click', function(){
     greetingText.innerText = "The news doesn't seem to be on...actually...none of these stations seem to be working..."
+    static.play()
 })
 headDownstairs.addEventListener('click', function(){
     cityLevel.style.display = 'none'
     streetLevel.style.display = 'flex'
     streetLevel.style.animation = 'fade-in 2s ease forwards'
     buttonContainer2.style.animation = 'fade-in 1s ease 1s forwards'
+    doorOpen.play()
+    static.pause()
 })
 //street-level
 headInside.addEventListener('click', function(){
     streetText.innerText = "Hmm...the front door seems to have closed behind you.  Funny, you don't recall them locking your building during the daytime..."
+    knock.play()
 })
 headGroceryStore.addEventListener('click', function(){
     streetLevel.style.display = 'none'
     walkingLevel.style.display = 'flex'
     walkingLevel.style.animation = 'fade-in 2s ease forwards'
     buttonContainer3.style.animation = 'fade-in 1s ease 1s forwards'
+    steps1.play()
+    knock.pause()
 })
 // walking-level
 headAlley.addEventListener('click', function(){
@@ -210,12 +249,15 @@ headAlley.addEventListener('click', function(){
     alleyLevel.style.animation = 'fade-in 2s ease forwards'
     buttonContainer4.style.animation = 'fade-in 1s ease 1s forwards'
     character.zombieAware = true 
+    zombieGroan.play()
+    steps1.pause()
 })
 headGroceryStore2.addEventListener('click', function(){
     walkingLevel.style.display = 'none'
     groceryOutside.style.display = 'flex'
     groceryOutside.style.animation = 'fade-in 2s ease forwards'
     buttonContainer5.style.animation = 'fade-in 1s ease 1s forwards'
+    steps1.play()
 })
 //alley-level
 runAlley.addEventListener('click', function(){
@@ -233,12 +275,15 @@ headHome.addEventListener('click', function(){
     walkingLevel.style.display = 'flex'
     walkingLevel.style.animation = 'fade-in 2s ease forwards'
     buttonContainer3.style.animation = 'fade-in 1s ease 1s forwards'
+    steps1.play()
 })
 enterStore.addEventListener('click', function(){
     groceryOutside.style.display = 'none';
     groceryEntrance.style.display = 'flex'
     groceryEntrance.style.animation = 'fade-in 2s ease forwards'
     buttonContainer6.style.animation = 'fade-in 1s ease 1s forwards';
+    birds.pause()
+    metalDoor.play()
 })
 
 //grocery entrance events
@@ -252,11 +297,13 @@ explorePharmacy1.addEventListener('click', function(){
         pharmacyText.innerText = "What are these things?  You can choose to attack, or sneak away..."
     }
     buttonContainer7.style.animation = 'fade-in 1s ease 2s forwards';
+    zombieBreath.play()
 })
 leaveStore1.addEventListener('click', function(){
     groceryEntrance.style.display = 'none';
     groceryOutside.style.display = 'flex'
     groceryOutside.style.animation = 'fade-in 2s ease forwards'
+    birds.play()
 })
 exploreCooking1.addEventListener('click', function(){
     groceryEntrance.style.display = 'none';
@@ -272,17 +319,21 @@ exploreCooking2.addEventListener('click', function(){
     cooking.style.animation = 'fade-in 2s ease forwards'
     character.haveWeapon = true;
     buttonContainer8.style.animation = 'fade-in 1s ease 1s forwards';
+    zombieBreath.pause()
 })
 leaveStore2.addEventListener('click', function(){
     pharmacy.style.display = 'none';
     groceryOutside.style.display = 'flex'
     groceryOutside.style.animation = 'fade-in 2s ease forwards'
+    birds.play()
+    zombieBreath.pause()
 })
 
 findMedicine.addEventListener('click', function(){
     findMedicineText.innerText = "(You've found medicine that will increase your health! +5 HP)";
     character.hp += 5;
-    hpCount.innerText = character.hp
+    hpCount.innerText = character.hp;
+    upgrade.play();
 })
 
 //Cooking aisle events
@@ -290,6 +341,7 @@ explorePharmacy3.addEventListener('click', function(){
     cooking.style.display = 'none';
     pharmacy.style.display = 'flex'
     pharmacy.style.animation = 'fade-in 2s ease forwards'
+    zombieBreath.play()
     if (character.zombieAware == true){
         pharmacyText.innerText = "More of them!  There's no way you can take them on...not without a weapon...better check the rest of the store..."
     } else {
@@ -301,18 +353,34 @@ leaveStore3.addEventListener('click', function(){
     cooking.style.display = 'none';
     groceryOutside.style.display = 'flex';
     groceryOutside.style.animation = 'fade-in 2s ease forwards';
+    birds.play()
 })
 findWeapon.addEventListener('click', function(){
     findWeaponText.innerText = "(You've found a weapon! Now let's go bash some zombies!)"
     character.haveWeapon = true
     findWeapon.style.display = 'none'
+    upgrade.play()
 })
 searchAlley.addEventListener('click', function(){
+    birds.pause()
+    cat.play()
+    setTimeout(function(){
+        victory.play()
+    }, 1500)
+    if (character.hp > 10){
+        score.innerHTML = `Wow, you're better off than when you started with ${character.hp} hit points! Congratulations!`
+    } else if (character.hp <= 10 && character.hp >= 5){
+        score.innerHTML = `Great job, you finished the scenario with ${character.hp} hit points!`
+    } else if (character.hp < 5 && character.hp > 1){
+        score.innerHTML = `Not bad, you finished the scenario with ${character.hp} hit points!`
+    } else {
+        score.innerHTML = `You barely made it with only ${character.hp} hit point left!`
+    }
     alleyLevel.style.display = 'none';
     endLevel.style.display = 'flex';
     endLevel.style.animation = 'fade-in 2s ease forwards';
     buttonContainer9.style.animation = 'fade-in 1s ease 1s forwards';
-    complete.innerText = 'Thanks for playing!'
+    complete.innerText = 'Thanks for playing!';
 })
 
 //game over events
@@ -328,4 +396,6 @@ sayonara.addEventListener('click', function(){
     gameOver.style.display = 'none'
     hpContainer.style.display = 'none'
     titleContainer.style.display = 'flex'
+    character.hp = 10;
+    hpCount.innerText = character.hp
 })
